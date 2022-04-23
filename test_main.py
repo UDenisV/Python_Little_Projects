e5fa22ddc206318a38cba91c8274479a382bec07
UDenisV <100012384+UDenisV@users.noreply.github.com>
2022-04-23 14:26:51 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 import main
 
-def test_is_factor():
+def test_is_factor1():
     if main.is_factor(3, 12) == True:
         print("Test is_factor(a, b) is OK")
     else:
@@ -8,12 +8,28 @@ def test_is_factor():
     print("Result:", main.is_factor(3, 12))
     print()
 
-def test_is_mulltiple():
+def test_is_factor2():
+    if main.is_factor(4, 7) == False:
+        print("Test is_factor(a, b) is OK")
+    else:
+        print("Test is_factor(a, b) is Fail")
+    print("Result:", main.is_factor(4, 7))
+    print()
+
+def test_is_mulltiple1():
     if main.is_mulltiple(15, 5) == True:
         print("Test is_mulltiple(a, b) is OK")
     else:
         print("Test is_mulltiple(a, b) is Fail")
     print("Result:", main.is_mulltiple(15, 5))
+    print()
+
+def test_is_mulltiple2():
+    if main.is_mulltiple(9, 5) == False:
+        print("Test is_mulltiple(a, b) is OK")
+    else:
+        print("Test is_mulltiple(a, b) is Fail")
+    print("Result:", main.is_mulltiple(9, 5))
     print()
 
 def test_num_digits():
@@ -40,8 +56,10 @@ def test_sum_of_squares_of_digits():
     print("Result:", main.sum_of_squares_of_digits(987))
     print()
 
-test_is_factor()
-test_is_mulltiple()
+test_is_factor1()
+test_is_factor2()
+test_is_mulltiple1()
+test_is_mulltiple2()
 test_num_digits()
 test_is_prime()
 test_sum_of_squares_of_digits()
